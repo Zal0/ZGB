d4a4a01d88bd1d6a09816c511f523fc15e61efd3
Zalo <cebolleto@gmail.com>
2016-07-02 09:07:31 +0200

    scroll first tests

--- galaxy/src/main.c
+++ galaxy/src/main.c
@@ -5,18 +5,61 @@
 #include "Sprite.h"
 #include "Keys.h"
 
-#include "smile.h"
-
 #include <gb/drawing.h>
+
+#include "smile.h"
+#include "tileTest.h"
+#include "map.h"
+
+#include "string.h"
+
+#define UPDATE_TILE(X, Y, T) set_bkg_tiles(X, Y, 1, 1, T)
+
+
+
+
+void ScrollUpdateRow(UINT16 x, UINT16 y, unsigned char* map, UINT8 map_w) {
+	UINT8 i = 0;
+
+	map = &map[map_w * y + x];
+	for(i = 0; i != 21u; ++i) {
+		UPDATE_TILE(x + i, y, map);
+		map += 1;
+	}
+}
+
+void ScrollUpdateColumn(UINT16 x, UINT16 y, unsigned char* map, UINT8 map_w) {
+	UINT8 i = 0;
+
+	map = &map[map_w * y + x];
+	for(i = 0; i != 19; ++i) {
+		UPDATE_TILE(x, y + i, map);
+		map += map_w;
+	}
+}
+
 
 UINT8 anim[] = {2, 0, 1};
 struct Sprite sprite;
-
 void Start() {
-	SPRITES_8x16;
+	UINT8 i = 0;
+	UINT16 cnt = 0;
+	UINT8* scroll_transfer[32];
+
 	SWITCH_ROM_MBC1(2);
+
+	SPRITES_8x16;
 	set_sprite_data(0, 8, smile);
 	SHOW_SPRITES;
+
+	set_bkg_data(0, 4, TileLabel);
+	/*for(i = 0u; i != 19u && i != mapTestHeight; ++i) {
+		ScrollUpdateRow(40u, i, mapTest, mapTestWidth);
+	}*/
+	for(i = 0u; i != 21u && i != mapTestWidth; ++i) {
+		ScrollUpdateColumn(i, 0u, mapTest, mapTestWidth);
+	}
+	SHOW_BKG;
 
 	InitSprite(&sprite, FRAME_16x16, 0);
 	SetSpriteAnim(&sprite, anim);
@@ -36,9 +79,9 @@ void Update() {
 
 	DrawSprite(&sprite);
 
-	DrawFrame(FRAME_16x16, 1, 50u, 50u);
-	DrawFrame(FRAME_16x16, 1, 30u, 70u);
-	DrawFrame(FRAME_16x16, 1, 80u, 100u);
+	DrawFrame(FRAME_16x16, 0, 50u, 50u);
+	DrawFrame(FRAME_16x16, 0, 30u, 70u);
+	DrawFrame(FRAME_16x16, 0, 80u, 100u);
 
 	//gotogxy(0, 0);
 	//gprintf("%u,%u", sprite.x, sprite.y);
