7ab217c8e9180eb154b121353cd2e169ba4a6394
Zalo <cebolleto@gmail.com>
2016-07-03 20:57:18 +0200

    I forgot this

---
+++ bitbit3/src/StateWin.c
@@ -0,0 +1,23 @@
+#include "StateWin.h"
+
+#include <gb/gb.h>
+#include "main.h"
+#include "Scroll.h"
+#include "Keys.h"
+
+#include "winmap.h"
+#include "splashtiles.h"
+
+void StartStateWin() {
+	SWITCH_ROM_MBC1(2);
+
+	set_bkg_data(0, 104, splashtiles);
+	InitScroll(winmapWidth, winmapHeight, winmap, 0, 0, 0, 2);
+	SHOW_BKG;
+}
+
+void UpdateStateWin() {
+	if(KEY_TICKED(J_START)) {
+		SetState(STATE_MENU);
+	}
+}
