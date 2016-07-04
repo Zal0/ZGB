c3cc8d4ad803f5609ca3f56fda0629c6cc6f22f3
Zalo <cebolleto@gmail.com>
2016-07-04 16:26:34 +0200

    ingame music. Shitty code

--- bitbit3/src/StateGame.c
+++ bitbit3/src/StateGame.c
@@ -15,6 +15,8 @@
 
 #include "main.h"
 
+#include "gbt_player.h"
+
 const UINT8 collision_tiles[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 47, 48, 53, 0};
 
 //Princes anims
--- bitbit3/src/main.c
+++ bitbit3/src/main.c
@@ -12,7 +12,7 @@
 #include "StateGameOver.h"
 #include "StateWin.h"
 
-STATE next_state = STATE_MENU;//STATE_DISCLAIMER;// STATE_GAME;
+STATE next_state = STATE_MENU;// STATE_GAME; //STATE_MENU;
 
 STATE current_state = N_STATES;
 UINT8 state_running = 0;
@@ -46,26 +46,43 @@ void SetState(STATE state) {
 	next_state = state;
 }
 
+#include "gbt_player.h"
+extern const unsigned char * song_Data[];
+
 void main() {
+	disable_interrupts();       // desactiva las interrupciones
+	add_VBL(gbt_update);        // actualiza el player, ha de ejecutarse cada frame (cambia a banco 1)
+	set_interrupts(VBL_IFLAG);  // añade la interrupcion VBL
+	enable_interrupts();        // activa las interrupciones
+
 	while(1) {
 		while (state_running) {
 			wait_vbl_done();
-			RefreshScroll();
-		
+
 			ResetOAM();
 			UPDATE_KEYS();
 
 			Update();
+			RefreshScroll();
 			FlushOAM();
+
+			//gbt_update();
 		}
 
 		DISPLAY_OFF
+		gbt_stop();
 		ResetOAM();
 		FlushOAM();
 		last_sprite_loaded = 0;
 		state_running = 1;
 		current_state = next_state;
 		Start();	
+
+		if(current_state == STATE_GAME) {
+			gbt_play(song_Data, 3, 7);  // reproduce la cancion, en banco indicado y a la velocidad dada
+			gbt_loop(0);                // activa/desactiva el loop
+		}
+
 		wait_vbl_done();
 		DISPLAY_ON;
 	}
