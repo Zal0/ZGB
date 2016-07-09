49d53b760bcc9d2d4ec2c2030da083ef9f3678e2
Zalo <cebolleto@gmail.com>
2016-07-09 13:51:06 +0200

    simple stack implementation

--- bitbit3/src/StateTests.c
+++ bitbit3/src/StateTests.c
@@ -1,15 +1,15 @@
 #include "StateTests.h"
 
 #include <gb/gb.h>
-
-#include "Vector.h"
 #include <stdio.h>
 
-DECLARE_VECTOR(v_test, 10)
+#include "Vector.h"
+#include "Stack.h"
 
 void StartStateTests() {
 	UINT8 i;
 
+	/*DECLARE_VECTOR(v_test, 10);
 	VectorAdd(v_test, 0u);
 	VectorAdd(v_test, 3u);
 	VectorAdd(v_test, 2u);
@@ -23,7 +23,23 @@ void StartStateTests() {
 	PRINT_VECTOR(v_test);
 
 	VectorAdd(v_test, 5);
-	PRINT_VECTOR(v_test);
+	PRINT_VECTOR(v_test);*/
+
+	/*DECLARE_STACK(s_test, 10);
+	StackPush(s_test, 0);
+	StackPush(s_test, 3);
+	StackPush(s_test, 2);
+	StackPush(s_test, 5);
+	PRINT_STACK(s_test);
+
+	printf("Pop:%u\n", (UINT16)(StackPop(s_test)));
+	PRINT_STACK(s_test);
+
+	printf("Pop:%u\n", (UINT16)(StackPop(s_test)));
+	PRINT_STACK(s_test);
+
+	printf("Pop:%u\n", (UINT16)(StackPop(s_test)));
+	PRINT_STACK(s_test);*/
 }
 
 void UpdateStateTests() {
