f88aeaff1661373d70c5948c7f28eac68e89582a
Zalo <cebolleto@gmail.com>
2016-07-13 18:13:54 +0200

    fixed sprites not being disabled when going offscreen

--- common/src/Frame.c
+++ common/src/Frame.c
@@ -2,34 +2,40 @@
 #include "OAMManager.h"
 #include "Scroll.h"
 
+#define SCREENWIDTH_PLUS_32 192 //160 + 32
+#define SCREENHEIGHT_PLUS_32 176 //144 + 32
+
 void DrawFrame(UINT8 oam_idx, FrameSize size, int idx, UINT16 x, UINT16 y, UINT8 flags){
 	x = x - scroll_x;
 	y = y - scroll_y;
 
-	//It might sound stupid adding 32 in both sides but remember the values are unsigned! (and truncated very likely)
-	if((x + 32 < SCREENWIDTH + 32) && (y + 32 < SCREENHEIGHT + 32)) {
-		switch(size) {
-			case FRAME_8x8:
-				DrawOAMSprite(oam_idx, idx, OAM_SIZE_8x8, x + 8u, y + 16u, flags);
-				break;
+	//It might sound stupid adding 32 in both sides but remember the values are unsigned! (and maybe truncated after substracting scroll_)
+	if((x + 32u > SCREENWIDTH_PLUS_32) || (y + 32 > SCREENHEIGHT_PLUS_32)) {
+		x = 200u;
+		y = 200u;
+	}
+	
+	switch(size) {
+		case FRAME_8x8:
+			DrawOAMSprite(oam_idx, idx, OAM_SIZE_8x8, x + 8u, y + 16u, flags);
+			break;
 
-			//case FRAME_8x16:
-				//TODO
-			//	break;
+		//case FRAME_8x16:
+			//TODO
+		//	break;
 
-			case FRAME_16x16:
-				if(flags & OAM_VERTICAL_FLAG){
-					DrawOAMSprite(oam_idx,     (idx << 2u) + 2u, OAM_SIZE_16x16, x + 8u,      y + 16u, flags);
-					DrawOAMSprite(oam_idx + 1, (idx << 2u),      OAM_SIZE_16x16, x + 8u + 8u, y + 16u, flags);
-				} else {
-					DrawOAMSprite(oam_idx,     (idx << 2u),      OAM_SIZE_16x16, x + 8u,      y + 16u, flags);
-					DrawOAMSprite(oam_idx + 1, (idx << 2u) + 2u, OAM_SIZE_16x16, x + 8u + 8u, y + 16u, flags);
-				}
-				break;
+		case FRAME_16x16:
+			if(flags & OAM_VERTICAL_FLAG){
+				DrawOAMSprite(oam_idx,     (idx << 2u) + 2u, OAM_SIZE_16x16, x + 8u,      y + 16u, flags);
+				DrawOAMSprite(oam_idx + 1, (idx << 2u),      OAM_SIZE_16x16, x + 8u + 8u, y + 16u, flags);
+			} else {
+				DrawOAMSprite(oam_idx,     (idx << 2u),      OAM_SIZE_16x16, x + 8u,      y + 16u, flags);
+				DrawOAMSprite(oam_idx + 1, (idx << 2u) + 2u, OAM_SIZE_16x16, x + 8u + 8u, y + 16u, flags);
+			}
+			break;
 
-			//case FRAME_32x32:
-				//TODO
-			//	break;
-		}
+		//case FRAME_32x32:
+			//TODO
+		//	break;
 	}
 }
