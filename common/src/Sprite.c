2995c54d2bdd11f4e97a2f617bda7c3699be98ec
Zalo <cebolleto@gmail.com>
2016-07-14 20:36:02 +0200

    replaced all % by & with is supposed to be faster

--- common/src/Sprite.c
+++ common/src/Sprite.c
@@ -64,7 +64,7 @@ UINT8 TranslateSprite(struct Sprite* sprite, INT8 x, INT8 y) {
 			
 				for(i = 0u; i != n_its; ++i, tile += scroll_tiles_w) {
 					if(scroll_collisions[*tile] == 1u) {
-						x -= (start_x % (UINT16)8u) + 1;
+						x -= (start_x & (UINT16)0x7u); // & 0x7 is % 8
 						ret = *tile;
 					}
 				}
@@ -79,7 +79,7 @@ UINT8 TranslateSprite(struct Sprite* sprite, INT8 x, INT8 y) {
 			
 				for(i = 0u; i != n_its; ++i, tile += scroll_tiles_w) {
 					if(scroll_collisions[*tile] == 1u) {
-						x = (INT16)x + (8u - (start_x % (UINT16)8u));
+						x += (INT16)8 - (start_x & 7);
 						ret = *tile;
 					}
 				}
@@ -94,7 +94,7 @@ UINT8 TranslateSprite(struct Sprite* sprite, INT8 x, INT8 y) {
 			
 				for(i = 0u; i != n_its; ++i, tile += 1u) {
 					if(scroll_collisions[*tile] == 1u) {
-						y -= (start_y % (UINT16)8u);
+						y -= (start_y & (UINT16)7u);
 						ret = *tile;
 					}
 				}
@@ -109,7 +109,7 @@ UINT8 TranslateSprite(struct Sprite* sprite, INT8 x, INT8 y) {
 			
 				for(i = 0u; i != n_its; ++i, tile += 1u) {
 					if(scroll_collisions[*tile] == 1u) {
-						y = (INT16)y + (8u - (start_y % (UINT16)8u));
+						y += (INT16)8 - (start_y & 7);
 						ret = *tile;
 					}
 				}
