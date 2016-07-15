8a1c8d05ae70b3f49a73c0cec6be1eb7c2ff0bbb
Zalo <cebolleto@gmail.com>
2016-07-15 20:05:55 +0200

    small size bug

--- common/include/Vector.h
+++ common/include/Vector.h
@@ -3,7 +3,7 @@
 
 #include <gb/gb.h>
 
-#define DECLARE_VECTOR(NAME, NUM_ELEMS) UINT8 NAME[NUM_ELEMS] = {0}
+#define DECLARE_VECTOR(NAME, NUM_ELEMS) UINT8 NAME[NUM_ELEMS + 1] = {0}
 #define PRINT_VECTOR(VECTOR) printf("N:%u ={", (UINT16) VECTOR[0]); for(i = 1u; i != VECTOR[0] + 1; ++i) printf("%u, ", (UINT16)VECTOR[i]); printf("}\n");
 
 #define VectorGetElem(VECTOR, IDX) VECTOR[IDX + 1]
