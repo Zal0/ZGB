070fa21a74eaa59e30750c3f78c98c8d94c39c9d
Zalo <cebolleto@gmail.com>
2016-07-16 11:31:25 +0200

    real test for sprite manager

--- bitbit3/src/StateTests.c
+++ bitbit3/src/StateTests.c
@@ -9,39 +9,19 @@
 #include "Stack.h"
 
 void StartStateTests() {
-	UINT8 i;
+	/*UINT8 i;
+	DECLARE_STACK(s_test, 20);
+	DECLARE_VECTOR(v_test, 20);
 
-	/*DECLARE_VECTOR(v_test, 10);
-	VectorAdd(v_test, 0u);
-	VectorAdd(v_test, 3u);
-	VectorAdd(v_test, 2u);
-	VectorAdd(v_test, 1u);
-	PRINT_VECTOR(v_test);
+	for(i = 0; i != 20; ++i) {
+		StackPush(s_test, i);
+	}
 
-	VectorRemovePos(v_test, 2);
-	PRINT_VECTOR(v_test);
-
-	VectorRemovePos(v_test, 0);
-	PRINT_VECTOR(v_test);
-
-	VectorAdd(v_test, 5);
+	for(i = 0; i != 15; ++i) {
+		VectorAdd(v_test, StackPop(s_test));
+	}
+	PRINT_STACK(s_test);
 	PRINT_VECTOR(v_test);*/
-
-	/*DECLARE_STACK(s_test, 10);
-	StackPush(s_test, 0);
-	StackPush(s_test, 3);
-	StackPush(s_test, 2);
-	StackPush(s_test, 5);
-	PRINT_STACK(s_test);
-
-	printf("Pop:%u\n", (UINT16)(StackPop(s_test)));
-	PRINT_STACK(s_test);
-
-	printf("Pop:%u\n", (UINT16)(StackPop(s_test)));
-	PRINT_STACK(s_test);
-
-	printf("Pop:%u\n", (UINT16)(StackPop(s_test)));
-	PRINT_STACK(s_test);*/
 }
 
 void UpdateStateTests() {
