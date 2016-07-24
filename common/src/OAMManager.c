1846b9b265c2c02c16703b732073b81df951773d
Zalo <cebolleto@gmail.com>
2016-07-24 12:10:22 +0200

    removed unused addition

--- common/src/OAMManager.c
+++ common/src/OAMManager.c
@@ -14,6 +14,4 @@ void DrawOAMSprite(UINT8 oam_idx, UINT8 idx, UINT8 x, UINT8 y, UINT8 flags) {
 	move_sprite(oam_idx, x, y);
 	set_sprite_tile(oam_idx, idx);
 	set_sprite_prop(oam_idx, flags);
-
-	oam_idx ++;
 }
