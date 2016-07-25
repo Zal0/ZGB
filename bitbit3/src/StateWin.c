5a842c45a880a0d805b97271d5677aa04f3144d9
Zalo <cebolleto@gmail.com>
2016-07-25 20:50:40 +0200

    gameover music on win screen

--- bitbit3/src/StateWin.c
+++ bitbit3/src/StateWin.c
@@ -10,10 +10,14 @@ UINT8 bank_STATE_WIN = 2;
 #include "resources/winmap.h"
 #include "resources/splashtiles.h"
 
+extern const unsigned char * gameover_mod_Data[];
+
 void Start_STATE_WIN() {
 	set_bkg_data(0, 104, splashtiles);
 	InitScroll(winmapWidth, winmapHeight, winmap, 0, 2);
 	SHOW_BKG;
+
+	PlayMusic(gameover_mod_Data, 3, 0);
 }
 
 void Update_STATE_WIN() {
