038ca753dd36055448c91800d0152981fe8da570
Zalo <cebolleto@gmail.com>
2016-07-25 19:36:13 +0200

    aznar screen limits increased aznar will change direction when colliding with something

--- bitbit3/src/SpriteAznar.c
+++ bitbit3/src/SpriteAznar.c
@@ -15,21 +15,23 @@ void Start_SPRITE_AZNAR(struct Sprite* sprite) {
 
 	sprite->coll_x += 4;
 	sprite->coll_w -= 8;
-	sprite->lim_x = 96u;
-	sprite->lim_y = 96u;
+	sprite->lim_x = 160u;
+	sprite->lim_y = 160u;
 }
 
 void Update_SPRITE_AZNAR() {
 	if(sprite_manager_current_sprite->flags & OAM_VERTICAL_FLAG) {
 		//moving left
-		sprite_manager_current_sprite->x -= 1 << delta_time;
-		if(!scroll_collisions[GetScrollTile(((sprite_manager_current_sprite->x + sprite_manager_current_sprite->coll_x) >> 3), (sprite_manager_current_sprite->y >> 3) + 2u)]) {
+		if(TranslateSprite(sprite_manager_current_sprite, -1, 0)) {
+			sprite_manager_current_sprite->flags = 0u;
+		} else	if(!scroll_collisions[GetScrollTile(((sprite_manager_current_sprite->x + sprite_manager_current_sprite->coll_x) >> 3), (sprite_manager_current_sprite->y >> 3) + 2u)]) {
 			sprite_manager_current_sprite->flags = 0u;
 		}
 	} else {
 		//moving right
-		sprite_manager_current_sprite->x += 1 << delta_time;
-		if(!scroll_collisions[GetScrollTile(((sprite_manager_current_sprite->x + sprite_manager_current_sprite->coll_x + sprite_manager_current_sprite->coll_w) >> 3), (sprite_manager_current_sprite->y >> 3) + 2u)]) {
+		if(TranslateSprite(sprite_manager_current_sprite, +1, 0)) {
+			sprite_manager_current_sprite->flags |= OAM_VERTICAL_FLAG;
+		} else if(!scroll_collisions[GetScrollTile(((sprite_manager_current_sprite->x + sprite_manager_current_sprite->coll_x + sprite_manager_current_sprite->coll_w) >> 3), (sprite_manager_current_sprite->y >> 3) + 2u)]) {
 			sprite_manager_current_sprite->flags |= OAM_VERTICAL_FLAG;
 		}
 	}
