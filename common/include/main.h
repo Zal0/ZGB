b8a702c9d215172bde0859f07ac15902ba919944
Zalo <cebolleto@gmail.com>
2016-10-09 01:33:09 +0200

    properly initializing sprites

--- common/include/main.h
+++ common/include/main.h
@@ -9,7 +9,7 @@
 #define DECLARE_SPRITE(SPRITE_IDX) extern UINT8 bank_##SPRITE_IDX; void Start_##SPRITE_IDX(struct Sprite* sprite); void Update_##SPRITE_IDX(); void Destroy_##SPRITE_IDX()
 
 #define INIT_STATE(STATE_IDX)   stateBanks[STATE_IDX]   = bank_##STATE_IDX;  startFuncs[STATE_IDX]        = Start_##STATE_IDX;  updateFuncs[STATE_IDX]        = Update_##STATE_IDX
-#define INIT_SPRITE(SPRITE_IDX) spriteBanks[SPRITE_IDX] = bank_##SPRITE_IDX; spriteStartFuncs[SPRITE_IDX] = Start_##SPRITE_IDX; spriteUpdateFuncs[SPRITE_IDX] = Update_##SPRITE_IDX
+#define INIT_SPRITE(SPRITE_IDX) spriteBanks[SPRITE_IDX] = bank_##SPRITE_IDX; spriteStartFuncs[SPRITE_IDX] = Start_##SPRITE_IDX; spriteUpdateFuncs[SPRITE_IDX] = Update_##SPRITE_IDX; spriteDestroyFuncs[SPRITE_IDX] = Destroy_##SPRITE_IDX
 
 typedef void (*Void_Func_Void)();
 typedef void (*Void_Func_SpritePtr)(struct Sprite*);
