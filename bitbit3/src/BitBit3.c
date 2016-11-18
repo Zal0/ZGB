8465e41fc363dc5cc866b13b3b19d85780948491
Zalo <cebolleto@gmail.com>
2016-11-18 20:05:24 +0100

    pretty princess compiled with latest changes

--- bitbit3/src/BitBit3.c
+++ bitbit3/src/BitBit3.c
@@ -38,12 +38,12 @@ void InitStates() {
 }
 
 void InitSprites() {
-	INIT_SPRITE(SPRITE_PRINCESS, princess,  2, FRAME_16x16, 10 * 4);
-	INIT_SPRITE(SPRITE_ZURRAPA,  zurrapa,   0, FRAME_16x16,  2 * 4);
-	INIT_SPRITE(SPRITE_PARTICLE, particles, 2, FRAME_16x16,  6 * 4);
-	INIT_SPRITE(SPRITE_AXE,      princess,  2, FRAME_16x16, 10 * 4);
-	INIT_SPRITE(SPRITE_AZNAR,    aznar,     2, FRAME_16x16,  5 * 4);
-	INIT_SPRITE(SPRITE_FLAG,     flag,      0, FRAME_16x16,  2 * 4);
+	INIT_SPRITE(SPRITE_PRINCESS, princess,  2, FRAME_16x16, 10);
+	INIT_SPRITE(SPRITE_ZURRAPA,  zurrapa,   0, FRAME_16x16,  2);
+	INIT_SPRITE(SPRITE_PARTICLE, particles, 2, FRAME_16x16,  6);
+	INIT_SPRITE(SPRITE_AXE,      princess,  2, FRAME_16x16, 10);
+	INIT_SPRITE(SPRITE_AZNAR,    aznar,     2, FRAME_16x16,  5);
+	INIT_SPRITE(SPRITE_FLAG,     flag,      0, FRAME_16x16,  2);
 }
 
 UINT8 GetTileReplacement(UINT8* tile_ptr, UINT8* tile) {
