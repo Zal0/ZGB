94eed3f296b5c55343af8638f6f9fd7b41217873
Zalo <cebolleto@gmail.com>
2016-11-23 19:45:22 +0100

    Added BIT manipulation functions

--- common/include/Math.h
+++ common/include/Math.h
@@ -5,4 +5,8 @@
 
 #define DISTANCE(A, B) (U_LESS_THAN(A, B) ? (B - A) : (A - B))
 
+#define SET_BIT(N, POS)   N |=   1 << POS
+#define UNSET_BIT(N, POS) N &= ~(1 << POS)
+#define GET_BIT(N, POS)   (1 & (N >> POS))
+
 #endif
