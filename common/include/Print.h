d9be685995a93b0e5d742ee379259f21fbbc7028
Zalo <cebolleto@gmail.com>
2016-11-26 10:32:41 +0100

    Macro for console initialization

--- common/include/Print.h
+++ common/include/Print.h
@@ -16,4 +16,19 @@ void IntToString (INT16  n, unsigned char* str);
 void Printf(const char* txt, ...);
 #define PRINT(X, Y, TXT) print_x = X; print_y  = Y; Printf(TXT);
 
+#ifdef NDEBUG 
+#define INIT_CONSOLE
+#else
+#define INIT_CONSOLE(FONT, FONT_BANK, NLINES) \
+	print_target = PRINT_WIN;\
+	print_x = 0;\
+	print_y = 0;\
+	font_idx = 255 - 45;\
+	InitScrollTiles(255 - 45, 45, FONT, FONT_BANK);\
+	WX_REG = 7;\
+  WY_REG = (144 - (NLINES << 3));\
+	SHOW_WIN;
+#endif
+
+
 #endif
