98d9c2a374085f02eb3e297c9803c98509779b7a
Zalo <cebolleto@gmail.com>
2017-04-22 17:49:04 +0200

    added functions for getting just one palette of some file

--- common/include/Palette.h
+++ common/include/Palette.h
@@ -1,17 +1,15 @@
 #ifndef PALETTE_H
 #define PALETTE_H
 
-
-                          
-#define PALETTE_FROM_HEADER(PAL) {                                \
-	PAL##CGBPal0c0, PAL##CGBPal0c1, PAL##CGBPal0c2, PAL##CGBPal0c3, \
-	PAL##CGBPal1c0, PAL##CGBPal1c1, PAL##CGBPal1c2, PAL##CGBPal1c3, \
-	PAL##CGBPal2c0, PAL##CGBPal2c1, PAL##CGBPal2c2, PAL##CGBPal2c3, \
-	PAL##CGBPal3c0, PAL##CGBPal3c1, PAL##CGBPal3c2, PAL##CGBPal3c3, \
-	PAL##CGBPal4c0, PAL##CGBPal4c1, PAL##CGBPal4c2, PAL##CGBPal4c3, \
-	PAL##CGBPal5c0, PAL##CGBPal5c1, PAL##CGBPal5c2, PAL##CGBPal5c3, \
-	PAL##CGBPal6c0, PAL##CGBPal6c1, PAL##CGBPal6c2, PAL##CGBPal6c3, \
-	PAL##CGBPal7c0, PAL##CGBPal7c1, PAL##CGBPal7c2, PAL##CGBPal7c3} 
+#define PALETTE_INDEX(PAL, IDX) PAL##CGBPal##IDX##c0, PAL##CGBPal##IDX##c1, PAL##CGBPal##IDX##c2, PAL##CGBPal##IDX##c3                       
+#define PALETTE_FROM_HEADER(PAL) PALETTE_INDEX(PAL, 0), \
+                                 PALETTE_INDEX(PAL, 1), \
+                                 PALETTE_INDEX(PAL, 2), \
+                                 PALETTE_INDEX(PAL, 3), \
+                                 PALETTE_INDEX(PAL, 4), \
+                                 PALETTE_INDEX(PAL, 5), \
+                                 PALETTE_INDEX(PAL, 6), \
+                                 PALETTE_INDEX(PAL, 7)
 
 //RGB defined in cgb.h has a << 0 that kills the compiler
 #define RGB2(r, g, b) ((UINT16)(r)) | (((UINT16)(g)) <<  5) | ((((UINT16)(b)) << 8) << 2);
