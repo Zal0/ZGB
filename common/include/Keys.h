7514400cfc6db0840d7f024ed70e7df410ea0bca
Zalo <cebolleto@gmail.com>
2017-07-03 19:34:16 +0200

    KEY_RELEASED macro

--- common/include/Keys.h
+++ common/include/Keys.h
@@ -9,6 +9,9 @@ extern UBYTE keys;
 #define UPDATE_KEYS() previous_keys = keys; keys = joypad()
  
 #define KEY_PRESSED(K) (keys & (K))
-#define KEY_TICKED(K) ((keys & (K)) && !(previous_keys & (K)))
+#define KEY_TICKED(K)   ((keys & (K)) && !(previous_keys & (K)))
+#define KEY_RELEASED(K) ((previous_keys & (K)) && !(keys & (K)))
+
+#define ANY_KEY_PRESSED (keys)
 
 #endif
