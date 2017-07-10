4bc69868c23d28693b57d1693a377f7b9cf8946d
Zalo <cebolleto@gmail.com>
2017-07-10 21:17:54 +0200

    directly manipulations OAMS instead of using the GBDK functions

--- common/include/OAMManager.h
+++ common/include/OAMManager.h
@@ -7,6 +7,15 @@ typedef enum {
 	OAM_HORIZONTAL_FLAG = 0x40
 } OAMFLAGS;
 
+struct OAMData {
+	UINT8 y;
+	UINT8 x;
+	UINT8 idx;
+	UINT8 flags;
+};
+extern UINT8* oams;
+#define OAM(A) ((struct OAMData*)(oams + (A << 2)))
+
 extern UINT8 last_sprite_loaded;
 
 UINT8 LoadSprite(UINT8 n_tiles, unsigned char* data, UINT8 bank, UINT8 frame_size, unsigned char* palette_idx);
--- common/src/OAMManager.c
+++ common/src/OAMManager.c
@@ -6,29 +6,34 @@ UINT8 sprites_pal[256];
 
 UINT8 LoadSprite(UINT8 n_tiles, unsigned char* data, UINT8 bank, UINT8 frame_size, unsigned char* palette_idx) {
 	UINT8 i;
-	UINT8 j;
+	UINT8* sprites_pal_ptr = &sprites_pal[last_sprite_loaded];
+	const UINT8 n_native_tiles = n_tiles << frame_size;
 
 	PUSH_BANK(bank);
 	set_sprite_data(last_sprite_loaded, n_tiles, data);
-	for(i = 0; i < n_tiles; i += (1 << frame_size)) {
-		for(j = 0; j < 1 << frame_size; ++j) {
-			sprites_pal[last_sprite_loaded + i + j] = palette_idx ? *(palette_idx) : 0; 
-		}
-		if(palette_idx)
-			palette_idx ++;
-	}
 	last_sprite_loaded += n_tiles;
+
+	for(i = 0; i != n_native_tiles; ++i, sprites_pal_ptr ++) {
+		if(palette_idx)
+			*sprites_pal_ptr = *(palette_idx + (i >> frame_size)); 
+		else
+			*sprites_pal_ptr = 0; 
+	}
 	POP_BANK;
 
 	return last_sprite_loaded - n_tiles;
 }
 
+UINT8* oams = (__REG)0xC000;
 
 void DrawOAMSprite(UINT8 oam_idx, UINT8 idx, UINT8 x, UINT8 y, UINT8 flags) {
-	move_sprite(oam_idx, x, y);
-	set_sprite_tile(oam_idx, idx);
+	UINT8* oam = (oams + (oam_idx << 2));
 #ifdef CGB
 	flags = sprites_pal[idx] | flags;
 #endif
-	set_sprite_prop(oam_idx, flags);
+
+	*(oam ++) = y;
+	*(oam ++) = x;
+	*(oam ++) = idx;
+	*(oam ++) = flags;
 }
