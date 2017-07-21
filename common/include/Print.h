9824ea343510838bfba152aa823a2d67cb9243b2
Zalo <cebolleto@gmail.com>
2017-07-21 22:16:18 +0200

    some small fixes

--- common/include/Print.h
+++ common/include/Print.h
@@ -27,11 +27,9 @@ void Printf(const char* txt, ...);
 #define DPRINT
 #else
 #define INIT_CONSOLE(FONT, FONT_BANK, NLINES) \
-	print_target = PRINT_WIN;\
+	INIT_FONT(FONT, FONT_BANK, PRINT_WIN) \
 	print_x = 0;\
 	print_y = 0;\
-	font_idx = 255 - 45;\
-	InitScrollTiles(255 - 45, 45, FONT, FONT_BANK);\
 	WX_REG = 7;\
   WY_REG = (144 - (NLINES << 3));\
 	scroll_h_border = NLINES << 3;\
