e1d83b045bb9b399853ac211defb324575bfc3f8
Zalo <cebolleto@gmail.com>
2018-08-01 19:28:03 +0200

    fixed bug when loading coloured sprites

--- common/src/OAMManager.c
+++ common/src/OAMManager.c
@@ -7,13 +7,12 @@ UINT8 sprites_pal[256];
 UINT8 LoadSprite(UINT8 n_tiles, unsigned char* data, UINT8 bank, UINT8 frame_size, unsigned char* palette_idx) {
 	UINT8 i;
 	UINT8* sprites_pal_ptr = &sprites_pal[last_sprite_loaded];
-	const UINT8 n_native_tiles = n_tiles << frame_size;
 
 	PUSH_BANK(bank);
 	set_sprite_data(last_sprite_loaded, n_tiles, data);
 	last_sprite_loaded += n_tiles;
 
-	for(i = 0; i != n_native_tiles; ++i, sprites_pal_ptr ++) {
+	for(i = 0; i != n_tiles; ++i, sprites_pal_ptr ++) {
 		if(palette_idx)
 			*sprites_pal_ptr = *(palette_idx + (i >> frame_size)); 
 		else
