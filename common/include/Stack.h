f30b88722b7aadc41927fb6c45e0d91ab25a692d
Zal0 <cebolleto@gmail.com>
2020-04-24 18:34:27 +0200

    Fixed Stack extra elem + print macros in stack and vector

--- common/include/Stack.h
+++ common/include/Stack.h
@@ -3,12 +3,17 @@
 
 #include <gb/gb.h>
 
-#define DECLARE_STACK(NAME, MAX_ELEMS) UINT8 NAME##STACK[MAX_ELEMS + 1]; UINT8* NAME = NAME##STACK - 1
-#define DECLARE_STAKCK_AND_PUSH(NAME, MAX_ELEMS, ELEM) UINT8 NAME##STACK[MAX_ELEMS + 1] = {ELEM}; UINT8* NAME = NAME##STACK
+#define DECLARE_STACK(NAME, MAX_ELEMS) UINT8 NAME##STACK[MAX_ELEMS]; UINT8* NAME = NAME##STACK - 1
+#define DECLARE_STAKCK_AND_PUSH(NAME, MAX_ELEMS, ELEM) UINT8 NAME##STACK[MAX_ELEMS] = {ELEM}; UINT8* NAME = NAME##STACK
 
-#define PRINT_STACK(STACK) printf("N:%u ={", (UINT16) STACK - STACK##STACK); for(UINT8* i = STACK##STACK + 1; i != STACK + 1; ++i) printf("%u, ", (UINT16)*i); printf("}\n");
+#define PRINT_STACK(NAME) Printf("%u(", (UINT16)NAME - (UINT16)NAME##STACK + 1);  \
+													for(UINT8* i = NAME##STACK; i != NAME + 1; ++i)         \
+														Printf("%u ", (UINT16)*i);                            \
+													Printf(")\n");
+
 
 #define StackPeek(STACK) (*STACK)
+
 #define StackPush(STACK, ELEM) (*(++STACK) = ELEM)
 #define StackPop(STACK) (*(STACK--))
 #define StackClear(NAME) NAME = NAME##STACK - 1
--- common/include/Vector.h
+++ common/include/Vector.h
@@ -4,7 +4,10 @@
 #include <gb/gb.h>
 
 #define DECLARE_VECTOR(NAME, NUM_ELEMS) UINT8 NAME[NUM_ELEMS + 1] = {0}
-#define PRINT_VECTOR(VECTOR) printf("N:%u ={", (UINT16) VECTOR[0]); for(i = 1u; i != VECTOR[0] + 1; ++i) printf("%u, ", (UINT16)VECTOR[i]); printf("}\n");
+#define PRINT_VECTOR(VECTOR) Printf("N:%u(", (UINT16) VECTOR[0]); \
+                             for(i = 1u; i != VECTOR[0] + 1; ++i)   \
+															Printf("%u ", (UINT16)VECTOR[i]);    \
+                             Printf(")");
 
 #define VectorGetElem(VECTOR, IDX) VECTOR[IDX + 1]
 #define IterateVector(VECTOR, IDX, ELEM) for(IDX = 0, ELEM = VECTOR[1]; IDX != VECTOR[0]; IDX ++, ELEM = VECTOR[IDX + 1])
