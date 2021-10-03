439eb6fc7b8405fe331ee4abcf5956032769eeba
Zal0 <cebolleto@gmail.com>
2021-10-03 09:30:03 +0200

    scroll_collisions_down with new collision system

--- common/src/Sprite.c
+++ common/src/Sprite.c
@@ -142,112 +142,15 @@ void DrawSprite() {
 
 
 #include "gb/bgb_emu.h"
-//#define OLD_COLLISION_SYSTEM
 unsigned char* tile_coll;
 UINT8 TranslateSprite(Sprite* sprite, INT8 x, INT8 y) {
 	BGB_PROFILE_BEGIN("BeginColl");
-#ifdef OLD_COLLISION_SYSTEM
-	UINT16 start_x, start_y, n_its,tmp;
-	UINT8 i;
-	UINT8 ret = 0;
 
-	if(scroll_map) {
-		if(x > 0) {
-			tmp = (sprite->x + sprite->coll_w - 1);
-			start_x = tmp + x;
-			if(((INT8)tmp & (INT8)0xF8) != ((INT8)start_x & (INT8)0xF8)) {
-				start_y = (sprite->y);
-				if(((start_y & 0xF000) | (start_x & 0xF000)) == 0u) {
-					n_its = ((start_y + sprite->coll_h - 1u) >> 3) - (start_y >> 3) + 1u;
-					PUSH_BANK(scroll_bank);
-					tile_coll = GetScrollTilePtr(start_x >> 3, start_y >> 3);
-			
-					for(i = 0u; i != n_its; ++i, tile_coll += scroll_tiles_w) {
-						if(scroll_collisions[*tile_coll] == 1u) {
-							x -= (start_x & (UINT16)7u) + 1;
-							ret = *tile_coll;
-							break;
-						}
-					}
-					POP_BANK;
-				}
-			}
-		}
-		else if(x < 0) {
-			tmp = sprite->x;
-			start_x = tmp + (INT16)x;
-			if(((INT8)tmp & (INT8)0xF8) != ((INT8)start_x & (INT8)0xF8)) {
-				start_y = (sprite->y);
-				if(((start_y & 0xF000) | (start_x & 0xF000)) == 0u) {
-					n_its = ((start_y + sprite->coll_h - 1u) >> 3) - (start_y >> 3) + 1u;
-					PUSH_BANK(scroll_bank);
-					tile_coll = GetScrollTilePtr(start_x >> 3, start_y >> 3);
-			
-					for(i = 0u; i != n_its; ++i, tile_coll += scroll_tiles_w) {
-						if(scroll_collisions[*tile_coll] == 1u) {
-							x = (INT16)x + (8u - (start_x & (UINT16)7u));
-							ret = *tile_coll;
-							break;
-						}
-					}
-					POP_BANK;
-				}
-			}
-		}
-		sprite->x += (INT16)x;
-	
-		if(y > 0) {
-			tmp = sprite->y + sprite->coll_h - 1;
-			start_y = tmp + y;
-			if(((INT8)tmp & (INT8)0xF8) != ((INT8)start_y & (INT8)0xF8)) {
-				start_x = (sprite->x);
-				if(((start_y & 0xF000) | (start_x & 0xF000)) == 0u) {
-					n_its = ((start_x + sprite->coll_w - 1u) >> 3) - (start_x >> 3) + 1u;
-					PUSH_BANK(scroll_bank);
-					tile_coll = GetScrollTilePtr(start_x >> 3, start_y >> 3);
-			
-					for(i = 0u; i != n_its; ++i, tile_coll += 1u) {
-						if(scroll_collisions[*tile_coll] == 1u || 
-							(scroll_collisions_down[*tile_coll] == 1u && //Tile that only checks collisions when going down
-							 scroll_collisions_down[*(tile_coll - scroll_tiles_w)] == 0) &&  //The one above is not collidable (so we can crate a big block putting some of there together)
-							 (((start_y - y) >> 3) != (start_y >> 3)) //The is entering the collidable tile in this moment
-						) {
-							y -= (start_y & (UINT16)7u) + 1;
-							ret = *tile_coll;
-							break;
-						}
-					}
-					POP_BANK;
-				}
-			}
-		}
-		else if(y < 0) {
-			tmp = sprite->y;
-			start_y = tmp + (INT16)y;
-			if(((INT8)tmp & (INT8)0xF8) != ((INT8)start_y & (INT8)0xF8)) {
-				start_x = (sprite->x);
-				if(((start_y & 0xF000) | (start_x & 0xF000)) == 0u) {
-					n_its = ((start_x + sprite->coll_w - 1u) >> 3) - (start_x >> 3) + 1u;
-					PUSH_BANK(scroll_bank);
-					tile_coll = GetScrollTilePtr(start_x >> 3, start_y >> 3);
-					for(i = 0u; i != n_its; ++i, tile_coll += 1u) {
-						if(scroll_collisions[*tile_coll] == 1u) {
-							y = (INT16)y + (8u - (start_y & (UINT16)7u));
-							ret = *tile_coll;
-							break;
-						}
-					}
-					POP_BANK;
-				}
-			}
-		}
-	}
-	sprite->y += (INT16)y;
-#else
 	UINT8 ret = 0;
 	INT16 pivot_x, pivot_y;
 	UINT8 start_tile_x, end_tile_x;
 	UINT8 start_tile_y, end_tile_y;
+	UINT8* scroll_coll_v;
 	UINT8 tmp;
 	if(x) {
 		if(x > 0) {
@@ -357,8 +260,9 @@ done_x:
 		PUSH_BANK(scroll_bank);
 		tile_coll = scroll_map + (scroll_tiles_w * start_tile_y + start_tile_x);
 		end_tile_x ++;
+		scroll_coll_v = y < 0 ? scroll_collisions : scroll_collisions_down;
 		for(tmp = start_tile_x; tmp != end_tile_x; tmp ++, tile_coll += 1) {
-			if(scroll_collisions[*tile_coll] == 1u) {
+			if(scroll_coll_v[*tile_coll] == 1u) {
 				if(y > 0) {
 					sprite->y = (start_tile_y << 3) - sprite->coll_h;
 				} else {
@@ -376,7 +280,6 @@ done_x:
 inc_y:
 	sprite->y += y;
 done_y:
-#endif
 
 BGB_PROFILE_END("EndColl ");
 	return ret;
