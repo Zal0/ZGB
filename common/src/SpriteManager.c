700b1c3c231ceca7aa5d4cb3ad0b1bace3531058
Zal0 <cebolleto@gmail.com>
2022-02-09 07:23:08 +0100

    removed memcmp since now it is included on GBDK

--- common/src/SpriteManager.c
+++ common/src/SpriteManager.c
@@ -48,19 +48,6 @@ void SpriteManagerReset() {
 	sprite_manager_removal_check = 0;
 }
 
-// memcmp while it is being added into GBDK
-int memcmp(const char *s1, const char *s2, int n) {
-	UINT8 i;
-	char* c1 = (char*)s1;
-	char* c2 = (char*)s2;
-	for(i = 0; i != n; ++i, ++c1, ++c2) {
-		if(*c1 != *c2){
-			return (*c1 > *c2) ? 1 : -1;
-		}
-	}
-	return 0;
-}
-
 extern UWORD ZGB_Fading_SPal[32];
 void SpriteManagerLoad(UINT8 sprite_type) {
 #ifdef CGB
