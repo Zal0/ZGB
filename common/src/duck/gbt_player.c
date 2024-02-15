68603c91d4f95aa562f478ba2040b1a450672c15
Toxa <untoxa@mail.ru>
2024-02-15 22:57:53 +0300

    fix megaduck builds (no music and sound yet)

--- common/src/duck/gbt_player.c
+++ common/src/duck/gbt_player.c
@@ -3,22 +3,22 @@
 
 #include <gbdk/platform.h>
 
-void gbt_play(void *data, UINT8 bank, UINT8 speed) {
+void gbt_play(void *data, UINT8 bank, UINT8 speed) OLDCALL {
 	data; bank; speed;
 }
 
-void gbt_pause(UINT8 pause) {
+void gbt_pause(UINT8 pause) OLDCALL {
 	pause;
 }
 
-void gbt_stop(void) {
+void gbt_stop(void) OLDCALL {
 }
 
-void gbt_loop(UINT8 loop) {
+void gbt_loop(UINT8 loop) OLDCALL {
 	loop;
 }
 
-void gbt_update(void) {
+void gbt_update(void) OLDCALL {
 }
 
 void gbt_enable_channels(UINT8 channel_flags) {
