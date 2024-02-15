c0f78396237702da326a61e3dd101208b1aa504d
Toxa <untoxa@mail.ru>
2024-02-15 23:49:09 +0300

    refactor PlayFx: use definitions instead of the immediate addresses (megaduck conversion logic still required)

--- common/src/duck/Sound.c
+++ common/src/duck/Sound.c
@@ -1,7 +1,24 @@
 #include <stdarg.h>
 
 #include "Sound.h"
+#include "Music.h"
+
+const UINT8 FX_REG_SIZES[] = {5, 4, 5, 4, 3};
+UINT8 * const FX_ADDRESS[] = {&NR10_REG, &NR21_REG, &NR30_REG, &NR41_REG, &NR50_REG};
+
+extern UINT8 music_mute_frames;
 
 void PlayFx(SOUND_CHANNEL channel, UINT8 mute_frames, ...) {
-	channel; mute_frames;
+	va_list list;
+
+	if(channel != CHANNEL_5) {
+		MUTE_CHANNEL(channel);
+	}
+	music_mute_frames = mute_frames;
+
+	va_start(list, mute_frames);
+	for(UINT8 i = 0, *reg = FX_ADDRESS[channel]; i < FX_REG_SIZES[channel]; ++i, ++reg) {
+		*reg = va_arg(list, INT16);
+	}
+	va_end(list);
 }
--- common/src/gb/Sound.c
+++ common/src/gb/Sound.c
@@ -3,16 +3,12 @@
 #include "Sound.h"
 #include "Music.h"
 
-typedef volatile UINT8 * REG_PTR;
-
 const UINT8 FX_REG_SIZES[] = {5, 4, 5, 4, 3};
-const UINT16 FX_ADDRESS[] = {0xFF10, 0xFF16, 0xFF1A, 0xFF20, 0xFF24};
+UINT8 * const FX_ADDRESS[] = {&NR10_REG, &NR21_REG, &NR30_REG, &NR41_REG, &NR50_REG};
 
 extern UINT8 music_mute_frames;
 
 void PlayFx(SOUND_CHANNEL channel, UINT8 mute_frames, ...) {
-	UINT8 i;
-	REG_PTR reg = (REG_PTR)FX_ADDRESS[channel];
 	va_list list;
 
 	if(channel != CHANNEL_5) {
@@ -21,7 +17,7 @@ void PlayFx(SOUND_CHANNEL channel, UINT8 mute_frames, ...) {
 	music_mute_frames = mute_frames;
 
 	va_start(list, mute_frames);
-	for(i = 0; i < FX_REG_SIZES[channel]; ++i, ++reg) {
+	for(UINT8 i = 0, *reg = FX_ADDRESS[channel]; i < FX_REG_SIZES[channel]; ++i, ++reg) {
 		*reg = va_arg(list, INT16);
 	}
 	va_end(list);
