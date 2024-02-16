d721b1a4600ec5745e6f775593e46dd4a582fb7d
Toxa <untoxa@mail.ru>
2024-02-16 19:04:29 +0300

    put out mirror_OAM[] placement into the makefile

--- common/src/OAMManager.c
+++ common/src/OAMManager.c
@@ -9,24 +9,21 @@
 //c000 + 160 = c0a0 but we cannot user c0a0, we need c100
 //c100 crashes (__at not working??) so I am gonna use df00-dfff (last 255 ram bytes)
 //This address overlaps with the stack eventually so I am also moving the stack to 0xDEFF (see the makefile)
-#ifndef OAM_MIRROR_ADDRESS
-	#define OAM_MIRROR_ADDRESS 0xdf00
-#endif
 
-OAM_item_t AT(OAM_MIRROR_ADDRESS) oam_mirror[40];
+extern OAM_item_t mirror_OAM[40];
 
 UINT8 next_oam_idx = 0;
 
 UINT8* oam  = (UINT8*)shadow_OAM;
 UINT8* oam0 = (UINT8*)shadow_OAM;
-UINT8* oam1 = (UINT8*)oam_mirror;
+UINT8* oam1 = (UINT8*)mirror_OAM;
 
 void SwapOAMs(void) {
 	hide_sprites_range(next_oam_idx, MAX_HARDWARE_SPRITES);
-	if ((_shadow_OAM_base = __render_shadow_OAM) == (UINT8)(OAM_MIRROR_ADDRESS >> 8)) { 
+	if ((_shadow_OAM_base = __render_shadow_OAM) == (UINT8)((UINT16)mirror_OAM >> 8)) { 
 		__render_shadow_OAM = (UINT8)((UINT16)(oam = (UINT8*)shadow_OAM) >> 8);
 	} else { 
-		__render_shadow_OAM = (UINT8)((UINT16)(oam = (UINT8*)oam_mirror) >> 8); 
+		__render_shadow_OAM = (UINT8)((UINT16)(oam = (UINT8*)mirror_OAM) >> 8); 
     	}
 	next_oam_idx = 0; 
 
@@ -35,13 +32,13 @@ void SwapOAMs(void) {
 }
 
 void ClearOAMs(void) {
-	__render_shadow_OAM = (UINT8)((UINT16)(shadow_OAM) >> 8);
+	__render_shadow_OAM = (UINT8)((UINT16)shadow_OAM >> 8);
 	hide_sprites_range(0, MAX_HARDWARE_SPRITES);
-	__render_shadow_OAM = (UINT8)(OAM_MIRROR_ADDRESS >> 8); 
+	__render_shadow_OAM = (UINT8)((UINT16)mirror_OAM >> 8); 
 	hide_sprites_range(0, MAX_HARDWARE_SPRITES);
 	next_oam_idx = 0;
 }
 
 void InitOAMs(void) {
-	memset(oam_mirror, 0, sizeof(oam_mirror));
+	memset(mirror_OAM, 0, sizeof(mirror_OAM));
 }
