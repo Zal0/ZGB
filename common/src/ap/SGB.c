165b0970ede4b181316739c73e6d2804fa45359a
Toxa <untoxa@mail.ru>
2024-02-18 00:45:46 +0300

    refactor set SGB border function (prevent on-screen garbage)

--- common/src/ap/SGB.c
+++ common/src/ap/SGB.c
@@ -7,68 +7,72 @@
 #define SGB_CHR_BLOCK0 0
 #define SGB_CHR_BLOCK1 1
 
-#define SGB_SCR_FREEZE 1
 #define SGB_SCR_UNFREEZE 0
+#define SGB_SCR_FREEZE   1
+#define SGB_SCR_BLACK    2
+#define SGB_SCR_COLOR0   3
 
-#define SGB_TRANSFER(A,B) map_buf[0]=(A),map_buf[1]=(B),sgb_transfer(map_buf) 
+#define SGB_TRANSFER(A,B) map_buf[0]=(((A) << 3) | 1),map_buf[1]=(B),sgb_transfer(map_buf) 
 
 void set_sgb_border(unsigned char * tiledata, size_t tiledata_size,
                     unsigned char * tilemap, size_t tilemap_size,
                     unsigned char * palette, size_t palette_size) {
-    if (sgb_check()) {
-        unsigned char map_buf[20];
-        memset(map_buf, 0, sizeof(map_buf));
 
-        SGB_TRANSFER((SGB_MASK_EN << 3) | 1, SGB_SCR_FREEZE); 
+	if (sgb_check()) {
+		UINT8 map_buf[16];
+		memset(map_buf, 0, sizeof(map_buf));
 
-        uint8_t __save_BGP = BGP_REG, __save_OBP0 = OBP0_REG, __save_OBP1 = OBP1_REG;
+		SGB_TRANSFER(SGB_MASK_EN, SGB_SCR_FREEZE); 
 
-        BGP_REG = OBP0_REG = OBP1_REG = 0xE4U;
-        SCX_REG = SCY_REG = 0U;
+		UINT8 __save_BGP = BGP_REG, __save_lcdc = LCDC_REG;
+		HIDE_SPRITES, HIDE_WIN, SHOW_BKG;
+		DISPLAY_ON;
 
-        uint8_t tmp_lcdc = LCDC_REG;
+		BGP_REG = DMG_PALETTE(DMG_WHITE, DMG_LITE_GRAY, DMG_DARK_GRAY, DMG_BLACK);
+		SCX_REG = SCY_REG = 0U;
 
-        HIDE_SPRITES, HIDE_WIN, SHOW_BKG;
-        DISPLAY_ON;
-        // prepare tilemap for SGB_BORDER_CHR_TRN (should display all 256 tiles)
-        uint8_t i = 0U;
-        for (uint8_t y = 0; y != 14U; ++y) {
-            uint8_t * dout = map_buf;
-            for (uint8_t x = 0U; x != 20U; ++x) {
-                *dout++ = i++;
-            }
-            set_bkg_tiles(0, y, 20, 1, map_buf);
-        }
-        memset(map_buf, 0, sizeof(map_buf));
+		// prepare tilemap for SGB_BORDER_CHR_TRN (should display all 256 tiles)
+		for (UINT8 y = 0, tile = 0; y != 14; y++) {
+			UINT8 * ptr = set_bkg_tile_xy(DEVICE_SCREEN_X_OFFSET, DEVICE_SCREEN_Y_OFFSET + y, tile++);
+			for (UINT8 x = 0; x != (DEVICE_SCREEN_WIDTH - 1); x++)
+				set_vram_byte(++ptr, tile++);
+			
+		} 
 
-        // transfer tile data
-        uint8_t ntiles = (tiledata_size > 256 * 32) ? 0 : tiledata_size >> 5;
-        if ((!ntiles) || (ntiles > 128U)) { 
-            set_bkg_data(0, 0, tiledata); 
-            SGB_TRANSFER((SGB_CHR_TRN << 3) | 1, SGB_CHR_BLOCK0);
-            if (ntiles) ntiles -= 128U; 
-            tiledata += (128 * 32);
-            set_bkg_data(0, ntiles << 1, tiledata); 
-            SGB_TRANSFER((SGB_CHR_TRN << 3) | 1, SGB_CHR_BLOCK1);
-        } else { 
-            set_bkg_data(0, ntiles << 1, tiledata); 
-            SGB_TRANSFER((SGB_CHR_TRN << 3) | 1, SGB_CHR_BLOCK0);
-        }
+		// transfer tile data
+		UINT8 ntiles = (tiledata_size > 256 * 32) ? 0 : tiledata_size >> 5;
+		if ((!ntiles) || (ntiles > 128U)) { 
+			set_bkg_data(0, 0, tiledata); 
+			SGB_TRANSFER(SGB_CHR_TRN, SGB_CHR_BLOCK0);
+			if (ntiles) ntiles -= 128U; 
+			tiledata += (128 * 32);
+			set_bkg_data(0, ntiles << 1, tiledata); 
+			SGB_TRANSFER(SGB_CHR_TRN, SGB_CHR_BLOCK1);
+		} else { 
+			set_bkg_data(0, ntiles << 1, tiledata); 
+			SGB_TRANSFER(SGB_CHR_TRN, SGB_CHR_BLOCK0);
+		}
 
-        // transfer map and palettes
-        set_bkg_data(0, (uint8_t)(tilemap_size >> 4), tilemap);
-        set_bkg_data(128, (uint8_t)(palette_size >> 4), palette);
-        SGB_TRANSFER((SGB_PCT_TRN << 3) | 1, 0);
+		// transfer map and palettes
+		set_bkg_data(0, (UINT8)(tilemap_size >> 4), tilemap);
+		set_bkg_data(128, (UINT8)(palette_size >> 4), palette);
+		SGB_TRANSFER(SGB_PCT_TRN, 0);
 
-        LCDC_REG = tmp_lcdc;
+		// clear SCREEN
+		memset(map_buf, 0, sizeof(map_buf));
+		set_bkg_data(0, 1, map_buf);
+		fill_bkg_rect(DEVICE_SCREEN_X_OFFSET, DEVICE_SCREEN_Y_OFFSET, DEVICE_SCREEN_WIDTH, DEVICE_SCREEN_HEIGHT, 0);
 
-        // clear SCREEN
-        fill_bkg_rect(0, 0, 20, 18, 0);
+		BGP_REG = __save_BGP;
 
-        BGP_REG = __save_BGP, OBP0_REG = __save_OBP0, OBP1_REG = __save_OBP1;
+		wait_vbl_done();
 
-        SGB_TRANSFER((SGB_MASK_EN << 3) | 1, SGB_SCR_UNFREEZE); 
-    }
+		SGB_TRANSFER(SGB_MASK_EN, SGB_SCR_UNFREEZE); 
+
+		if ((__save_lcdc & LCDCF_ON) == 0) DISPLAY_OFF;
+		LCDC_REG = __save_lcdc;
+
+	}
 }
 
 void LoadSGBBorder(UINT8 map_bank, struct MapInfo* map) {
