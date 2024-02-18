1b0a0c96c6566f4394e49a03f54ea411651edb23
Toxa <untoxa@mail.ru>
2024-02-18 21:01:26 +0300

    support for the recode tables in Printf(): that allows to save tiles or create larger fonts (no PNG conversion tool yet for the fonts)

--- common/include/Print.h
+++ common/include/Print.h
@@ -4,19 +4,24 @@
 #include <gbdk/platform.h>
 
 extern UINT8 last_tile_loaded;
-#define INIT_FONT(FONT, TARGET) print_target=(TARGET),font_offset=ScrollSetTiles(last_tile_loaded, BANK(FONT), &FONT)
+#define INIT_FONT(FONT, TARGET) InitDefaultRecode(),print_target=(TARGET),font_offset=ScrollSetTiles(last_tile_loaded, BANK(FONT), &FONT)
 #define INIT_FONT_NUMBERS(FONT, TARGET) INIT_FONT(FONT, TARGET),(((UINT8*)(&font_offset))[0]-=27)
 
 extern UINT8 print_x, print_y, print_target;
 extern INT8 scroll_h_border;
 extern UINT16 font_offset;
 
+#define RECODE_TABLE_SIZE 96
+extern unsigned char font_recode_table[RECODE_TABLE_SIZE];
+
+void InitDefaultRecode(void) BANKED;
+
 typedef enum {
 	PRINT_BKG,
 	PRINT_WIN
 } PRINT_TARGET;
 
-void Printf(const char* txt, ...);
+void Printf(const unsigned char* txt, ...);
 #define PRINT_POS(X, Y) print_x=(X),print_y=(Y)
 #define PRINT(X, Y, ...) PRINT_POS((X),(Y)),Printf(__VA_ARGS__)
 
--- common/src/Print.c
+++ common/src/Print.c
@@ -1,5 +1,8 @@
+#pragma bank 1
+
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "Print.h"
 #include "Scroll.h"
@@ -9,62 +12,53 @@ UINT8 print_y = 0;
 UINT8 print_target = PRINT_BKG;
 UINT16 font_offset = 0;
 
-void Printf(const char* txt, ...){
-	UINT8 idx = 0;
-	unsigned char c = 0;
-	unsigned char tmp[10];
+const unsigned char default_recode_table[RECODE_TABLE_SIZE] = {
+	0x00,0x25,0x00,0x00,0x00,0x00,0x00,0x00,0x27,0x28,0x00,0x00,0x00,0x29,0x2a,0x00,
+	0x1b,0x1c,0x1d,0x1e,0x1f,0x20,0x21,0x22,0x23,0x24,0x2b,0x00,0x00,0x00,0x00,0x2c,
+	0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,
+	0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1a,0x00,0x26,0x00,0x00,0x00,
+	0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,
+	0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1a,0x00,0x00,0x00,0x00,0x00
+};
+
+void InitDefaultRecode(void) BANKED {
+	memcpy(font_recode_table, default_recode_table, RECODE_TABLE_SIZE);
+}
+
+void Printf(const unsigned char* txt, ...) NONBANKED { 
+	static unsigned char tmp[10];
+	unsigned char c;
+	const unsigned char *ptr = txt; 
 	va_list list;
 
 	va_start(list, txt); 
-	while(*txt) {
-		if(*txt == ' ') {
-			c = 0;
-		} else if(*txt >= 'A' && *txt <= 'Z'){
-			c = 1 + *txt - 'A';
-		} else if(*txt >= 'a' && *txt <= 'z') {
-			c = 1 + *txt - 'a';
-		} else if(*txt >= '0' && *txt <= '9') {
-			c = 27 + *txt - '0';
-		} else {
-			switch(*txt) {
-				case  '!': c= 37; break;
-				case '\'': c= 38; break;
-				case  '(': c= 39; break;
-				case  ')': c= 40; break;
-				case  '-': c= 41; break;
-				case  '.': c= 42; break;
-				case  ':': c= 43; break;
-				case  '?': c= 44; break;
-				case  '%':
-					switch(*(txt + 1)) {
-						case 'd':
-						case 'i':
-							itoa(va_arg(list, INT16), tmp, 10);
-							Printf(tmp);
-							txt += 2;
-							continue;
-
-						case 'u':
-							uitoa(va_arg(list, INT16), tmp, 10);
-							Printf(tmp);
-							txt += 2;
-							continue;
-
-						case 's':
-							Printf(va_arg(list, char*));
-							txt += 2;
-							continue;
-					}
+	while(*ptr) {
+		c = (*ptr++) & 0x7fu; // support only ascii
+		if (c == '%') {
+			switch(c = *(ptr++) & 0x7fu) {
+				case 'd':
+				case 'i':
+					itoa(va_arg(list, INT16), tmp, 10);
+					Printf(tmp);
+					continue;
+				case 'u':
+					uitoa(va_arg(list, INT16), tmp, 10);
+					Printf(tmp);
+					continue;
+				case 's':
+					Printf(va_arg(list, char*));
+					continue;
+				default:
 					break;
 			}
 		}
+		c = font_recode_table[((c < ' ') ? 0u : (c - ' '))];
 		if(print_target == PRINT_BKG)
 			UpdateMapTile(print_target, 0x1F & (print_x + scroll_offset_x), 0x1F & (print_y + scroll_offset_y), font_offset, c, NULL);
 		else
 			UpdateMapTile(print_target, print_x, print_y, font_offset, c, NULL);
 
 		print_x ++;
-		txt ++;
 	}
 	va_end(list);
 }
