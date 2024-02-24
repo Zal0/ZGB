b1d20239ba69e8cdee1bae64c73fa0c7f7dce933
Toxa <untoxa@mail.ru>
2024-02-24 22:01:07 +0300

    put INIT_HUD under ifdef

--- common/include/Scroll.h
+++ common/include/Scroll.h
@@ -9,12 +9,16 @@
 
 #define SPRITE_UNIQUE_ID(TILE_X, TILE_Y) ((0x00FF & TILE_X) | ((0xFF00 & (TILE_Y << 8))))
 
+#if defined(NINTENDO)
 #define INIT_HUD(MAP)\
 	GetMapSize(BANK(MAP), &MAP, 0, &scroll_h_border); \
 	scroll_h_border = scroll_h_border << 3; \
 	WX_REG = DEVICE_WINDOW_PX_OFFSET_X, WY_REG = (DEVICE_WINDOW_PX_OFFSET_Y + DEVICE_SCREEN_PX_HEIGHT) - scroll_h_border; \
 	hud_map_offset = LoadMap(1, 0, 0, BANK(MAP), &MAP); \
 	SHOW_WIN;
+#else
+#define INIT_HUD(MAP)
+#endif
 
 #define INIT_BKG(MAP) LoadMap(0, 0, 0, BANK(MAP), &MAP)
 
