1bf25f787c1c81a7634b20966f1daa7d54976fe2
Toxa <untoxa@mail.ru>
2024-03-11 20:42:43 +0300

    remove SMS version of PlayFx(), because now we have the VGM SFX engine

--- common/src/sms/Sound.c
+++ common/src/sms/Sound.c
@@ -3,19 +3,6 @@
 #include "Sound.h"
 #include "Music.h"
 
-extern UINT8 music_mute_frames;
-
 void PlayFx(SOUND_CHANNEL channel, UINT8 mute_frames, ...) {
-	va_list list;
-
-	if(channel != CHANNEL_5) {
-		MUTE_CHANNEL(channel);
-	}
-	music_mute_frames = mute_frames;
-
-	va_start(list, mute_frames);
-	for(UINT8 i = va_arg(list, INT16); i != 0 ; --i) {
-		PSG = va_arg(list, INT16);
-	}
-	va_end(list);
+	channel; mute_frames;
 }
