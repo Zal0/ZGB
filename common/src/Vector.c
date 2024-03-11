6da7872564c3ba74cecd6cfc75dc8bdcd802d06d
Toxa <untoxa@mail.ru>
2024-03-11 20:33:11 +0300

    remove slower VectorAdd() for the faster inline macro

--- common/src/Vector.c
+++ common/src/Vector.c
@@ -2,10 +2,6 @@
 
 #include "Vector.h"
 
-void VectorAdd(UINT8* v, UINT8 elem) {
-	v[++ v[0]] = elem;
-}
-
 void VectorRemovePos(UINT8* v, UINT8 pos) {
 	memcpy(&v[pos + 1], &v[pos + 2], v[0] - pos);
 	v[0] --;
