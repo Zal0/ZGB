5c20db88107fe478c45530cc3191d20f82c57d1b
Toxa <untoxa@mail.ru>
2024-03-14 19:32:47 +0300

    indent with tabs for consistency with the rest of the code

--- common/src/gg/sms_set_native_tiles_flip.s
+++ common/src/gg/sms_set_native_tiles_flip.s
@@ -1,63 +1,63 @@
-        .include        "global.s"
+	.include "global.s"
 
-        .title  "VRAM utilities"
-        .module VRAMUtils
-        .area   _HOME
+	.title   "VRAM utilities"
+	.module  VRAMUtils
+	.area    _HOME
 
-        .globl  _flip_recode_table
+	.globl   _flip_recode_table
 
 ; void set_sprite_native_data_flipx(uint8_t start, uint16_t ntiles, const void *src);
 _set_sprite_native_data_flipx::
 	ld h, #1
-        ld l, a
+	ld l, a
+
 ; void set_native_tile_data_flipx(uint16_t start, uint16_t ntiles, const void *src);
 _set_native_tile_data_flipx::
-        ld b, h
-        ld c, l
+	ld b, h
+	ld c, l
 
-        DISABLE_VBLANK_COPY        ; switch OFF copy shadow SAT
+	DISABLE_VBLANK_COPY        ; switch OFF copy shadow SAT
 
-        ld h, b
-        ld l, c
+	ld h, b
+	ld l, c
 
-        add hl, hl
-        add hl, hl
-        add hl, hl
-        add hl, hl
-        add hl, hl
+	add hl, hl
+	add hl, hl
+	add hl, hl
+	add hl, hl
+	add hl, hl
 
-        ld bc, #.VDP_VRAM
-        add hl, bc
+	ld bc, #.VDP_VRAM
+	add hl, bc
 
-        VDP_WRITE_CMD h, l
+	VDP_WRITE_CMD h, l
 
-        pop iy
-        ex (sp), iy
-                
-        ld h, #>_flip_recode_table
-        
-        ld c, #.VDP_DATA
-        inc d
-        inc e
-        jr 2$
+	pop iy
+	ex (sp), iy
+				
+	ld h, #>_flip_recode_table
+		
+	ld c, #.VDP_DATA
+	inc d
+	inc e
+	jr 2$
 
 1$:
-        ld b, #32
+	ld b, #32
 3$:        
-        ld l, 0(iy)
-        inc iy
+	ld l, 0(iy)
+	inc iy
 
-        outi
-        dec hl
-        jp nz, 3$
+	outi
+	dec hl
+	jp nz, 3$
 2$:
-        dec e
-        jp  nz, 1$
+	dec e
+	jp  nz, 1$
 
-        dec d
-        jp nz, 1$
-        
-        ENABLE_VBLANK_COPY         ; switch ON copy shadow SAT
+	dec d
+	jp nz, 1$
+		
+	ENABLE_VBLANK_COPY         ; switch ON copy shadow SAT
 
-        ret
-
+	ret
