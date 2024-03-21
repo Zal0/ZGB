df9fdcbeed1dba8ed9296b39fdfeb852fcc32558
bbbbbr <bbbbbr@users.noreply.github.com>
2024-03-21 01:27:01 -0700

    vgm2data: fix include, function returns, etc

--- tools/vgm2data/src/vgm_psg.h
+++ tools/vgm2data/src/vgm_psg.h
@@ -1,7 +1,7 @@
-#ifndef _VGM_GB_H
-#define _VGM_GB_H
+#ifndef _VGM_PSG_H
+#define _VGM_PSG_H
 
 
 uint8_t vgm_process_psg_sound_data(uint8_t * p_buf_in, size_t buf_len_in, FILE * FOUT);
 
-#endif // _VGM_GB_H
+#endif // _VGM_PSG_H
