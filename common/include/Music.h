adf648d6a6d3405bae2ab50f5777677efca98133
Toxa <untoxa@mail.ru>
2024-03-29 10:33:44 +0300

    refactor music manager

--- common/include/Music.h
+++ common/include/Music.h
@@ -6,6 +6,7 @@
 #include "Sound.h"
 
 extern void* last_music;
+extern UINT8 last_music_bank;
 extern UINT8 stop_music_on_new_state;
 extern volatile UINT8 music_paused;
 
@@ -24,8 +25,8 @@ inline void INIT_SOUND(void) {
 #undef MUSIC_DRIVER_GBT
 #endif
 
-#define PauseMusic music_paused = 1, sfx_sound_cut()
-#define UnpauseMusic if (last_music) music_paused = 0
+#define PauseMusic music_paused = 1, sfx_sound_cut_mask(~music_mute_mask)
+#define ResumeMusic if (last_music) music_paused = 0
 
 #if defined(MUSIC_DRIVER_HUGE)
 	#include "hUGEDriver.h"
@@ -33,44 +34,37 @@ inline void INIT_SOUND(void) {
 	#define INIT_MUSIC
 	#define DECLARE_MUSIC(SONG) extern const void __bank_ ## SONG ## _uge; extern const hUGESong_t SONG ## _uge
 	#define PlayMusic(SONG, LOOP) __PlayMusic(&SONG ## _uge, (uint8_t)&__bank_ ## SONG ## _uge, 0)
-	#define StopMusic PauseMusic, last_music = NULL
+	#define StopMusic (sfx_sound_cut(), last_music_bank = SFX_STOP_BANK, last_music = NULL)
 
-	#define MUTE_CHANNEL(CHANNEL) if(last_music) hUGE_mute_mask = (CHANNEL)
-	#define UNMUTE_ALL_CHANNELS hUGE_reset_wave(),(hUGE_mute_mask = MUTE_MASK_NONE)
+	#define MuteMusicChannels(CHANNELS) (music_mute_mask = (CHANNELS))
 #elif defined(MUSIC_DRIVER_GBT)
 	#include "gbt_player.h"
 
-	extern UINT8 mute_channels;
 	#define INIT_MUSIC gbt_stop()
 	#define DECLARE_MUSIC(SONG) extern const void __bank_ ## SONG ## _mod_Data; extern const unsigned char * SONG ## _mod_Data[]
 	#define PlayMusic(SONG, LOOP) __PlayMusic(SONG ## _mod_Data, (uint8_t)&__bank_ ## SONG ## _mod_Data, LOOP)
-	#define StopMusic gbt_stop(); last_music = NULL
+	#define StopMusic (sfx_sound_cut(), last_music_bank = SFX_STOP_BANK, last_music = NULL)
 
-	#define MUTE_CHANNEL(CHANNEL) if (mute_channels != (CHANNEL)) (mute_channels = (CHANNEL)), gbt_enable_channels(0xF & ~(CHANNEL))
-	#define UNMUTE_ALL_CHANNELS (mute_channels = MUTE_MASK_NONE), gbt_enable_channels(0xF)
+	#define MuteMusicChannels(CHANNELS) (music_mute_mask = (CHANNELS))
 #elif defined(MUSIC_DRIVER_BANJO)
 	#include "banjo.h"
 
-	extern UINT8 mute_channels;
 	void __InitMusicDriver(void);
 	void __StopMusic(void);
-	void __SetMusicMuteMask(UINT8 mask);
+
 	#define INIT_MUSIC __InitMusicDriver()
 	#define DECLARE_MUSIC(SONG) extern const void __bank_ ## SONG ## _fur; extern const song_data_t SONG ## _fur
 	#define PlayMusic(SONG, LOOP) __PlayMusic(&SONG ## _fur, (uint8_t)&__bank_ ## SONG ## _fur, LOOP)
 	#define StopMusic __StopMusic()
 
-	#define MUTE_CHANNEL(CHANNEL) if (mute_channels != (CHANNEL)) (mute_channels = (CHANNEL)), __SetMusicMuteMask((CHANNEL))
-	#define UNMUTE_ALL_CHANNELS (mute_channels = MUTE_MASK_NONE), __SetMusicMuteMask(0)
+	#define MuteMusicChannels(CHANNELS) (music_mute_mask = (CHANNELS))
 #else
-	extern UINT8 mute_channels;
 	#define INIT_MUSIC
 	#define DECLARE_MUSIC(SONG) extern void SONG ## _undetected
 	#define PlayMusic(SONG, LOOP)
 	#define StopMusic
 
-	#define MUTE_CHANNEL(CHANNEL) (mute_channels = CHANNEL)
-	#define UNMUTE_ALL_CHANNELS (mute_channels = 0)
+	#define MuteMusicChannels(CHANNELS)
 #endif
 
 #endif
--- common/src/Music.c
+++ common/src/Music.c
@@ -16,19 +16,73 @@ UINT8 last_music_bank = SFX_STOP_BANK;
 
 UINT8 stop_music_on_new_state = 1;
 
-#if !defined(MUSIC_DRIVER_HUGE)
-UINT8 mute_channels = 0;
+
+#if defined(MUSIC_DRIVER_HUGE)
+inline void __SetMusicMuteMask(UINT8 mask) {
+	hUGE_mute_mask = mask;
+	hUGE_reset_wave();
+}
+#elif defined(MUSIC_DRIVER_GBT)
+inline void __SetMusicMuteMask(UINT8 mask) {
+	gbt_enable_channels(0xF & ~(mask));
+	gbt_reset_ch3_instrument();
+}
+#elif defined(MUSIC_DRIVER_BANJO)
+void __InitMusicDriver(void) NONBANKED {
+	UBYTE __save = CURRENT_BANK;
+	SWITCH_ROM(1);
+	banjo_check_hardware();
+	banjo_init(MODE_SN);
+	SWITCH_ROM(__save);
+}
+void __StopMusic(void) NONBANKED {
+	if (last_music_bank == SFX_STOP_BANK)
+		return;
+
+	UBYTE music_bank = last_music_bank; // save current music bank
+	last_music_bank = SFX_STOP_BANK;    // prevent running music updates from the interrupt
+	last_music = NULL;                  // clear the last music pointer
+
+	UBYTE __save = CURRENT_BANK, __save2 = MAP_FRAME2;
+	SWITCH_ROM(1);
+	SWITCH_ROM2(music_bank);
+	banjo_song_stop();
+	SWITCH_ROM2(__save2);
+	SWITCH_ROM(__save);
+}
+void __SetMusicMuteMask(UINT8 mask) NONBANKED {
+	if (last_music_bank == SFX_STOP_BANK)
+		return;
+	UBYTE __save = CURRENT_BANK, __save2 = MAP_FRAME2;
+	SWITCH_ROM(1);
+	SWITCH_ROM2(last_music_bank);
+	if (mask & CHANNEL_1) banjo_mute_song_channel(0); else banjo_unmute_song_channel(0);
+	if (mask & CHANNEL_2) banjo_mute_song_channel(1); else banjo_unmute_song_channel(1);
+	if (mask & CHANNEL_3) banjo_mute_song_channel(2); else banjo_unmute_song_channel(2);
+	if (mask & CHANNEL_4) banjo_mute_song_channel(3); else banjo_unmute_song_channel(3);
+	SWITCH_ROM2(__save2);
+	SWITCH_ROM(__save);
+}
+#else
+#define __SetMusicMuteMask(A)
 #endif
 
+
 void MUSIC_isr(void) NONBANKED {
+	static UINT8 old_mute_mask = ~MUTE_MASK_NONE;
+	if (old_mute_mask != music_mute_mask) {
+		__SetMusicMuteMask(music_mute_mask); 
+		old_mute_mask = music_mute_mask;
+	}
+
 	if (sfx_play_bank != SFX_STOP_BANK) {
-		MUTE_CHANNEL(music_mute_mask);
 		if (!sfx_play_isr()) {
-			UNMUTE_ALL_CHANNELS;
 #ifdef FORCE_CUT_SFX
 			sfx_sound_cut_mask(music_mute_mask);
 #endif
-			UNMUTE_ALL_CHANNELS;
+			__SetMusicMuteMask(MUTE_MASK_NONE);
+			music_mute_mask = old_mute_mask = MUTE_MASK_NONE;
+
 			sfx_priority = SFX_PRIORITY_MINIMAL;
 			sfx_play_bank = SFX_STOP_BANK;
 		}
@@ -61,7 +115,6 @@ void MUSIC_isr(void) NONBANKED {
 void __PlayMusic(void* music, UINT8 bank, UINT8 loop) NONBANKED {
 	bank; loop;
 	if(music != last_music) {
-		music_paused = 1;
 		last_music_bank = SFX_STOP_BANK;
 		UBYTE __save = CURRENT_BANK;
 #if defined(MUSIC_DRIVER_GBT)
@@ -84,44 +137,3 @@ void __PlayMusic(void* music, UINT8 bank, UINT8 loop) NONBANKED {
 	}
 	music_paused = 0;
 }
-
-#if defined(MUSIC_DRIVER_BANJO)
-void __InitMusicDriver(void) NONBANKED {
-	UBYTE __save = CURRENT_BANK;
-	SWITCH_ROM(1);
-	banjo_check_hardware();
-	banjo_init(MODE_SN);
-	SWITCH_ROM(__save);
-}
-
-void __StopMusic(void) NONBANKED {
-	if (last_music_bank == SFX_STOP_BANK)
-		return;
-
-	UBYTE music_bank = last_music_bank; // save current music bank
-	last_music_bank = SFX_STOP_BANK;    // prevent running music updates from the interrupt
-	last_music = NULL;                  // clear the last music pointer
-
-	UBYTE __save = CURRENT_BANK, __save2 = MAP_FRAME2;
-	SWITCH_ROM(1);
-	SWITCH_ROM2(music_bank);
-	banjo_song_stop();
-	SWITCH_ROM2(__save2);
-	SWITCH_ROM(__save);
-}
-
-void __SetMusicMuteMask(UINT8 mask) NONBANKED {
-	if (last_music_bank == SFX_STOP_BANK)
-		return;
-	UBYTE __save = CURRENT_BANK, __save2 = MAP_FRAME2;
-	SWITCH_ROM(1);
-	SWITCH_ROM2(last_music_bank);
-	if (mask & CHANNEL_1) banjo_mute_song_channel(0); else banjo_unmute_song_channel(0);
-	if (mask & CHANNEL_2) banjo_mute_song_channel(1); else banjo_unmute_song_channel(1);
-	if (mask & CHANNEL_3) banjo_mute_song_channel(2); else banjo_unmute_song_channel(2);
-	if (mask & CHANNEL_4) banjo_mute_song_channel(3); else banjo_unmute_song_channel(3);
-	SWITCH_ROM2(__save2);
-	SWITCH_ROM(__save);
-}
-
-#endif
