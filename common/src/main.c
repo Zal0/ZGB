2a62ad14a41ac13db4e973019d2613fb7660657e
Toxa <untoxa@mail.ru>
2024-03-30 16:27:44 +0300

    SMS/GG use unsafe VDP register access in the VBlank ISR

--- common/src/main.c
+++ common/src/main.c
@@ -40,12 +40,12 @@ void SetState(UINT8 state) {
 UINT8 vbl_count = 0;
 void VBL_isr(void) {
 	vbl_count ++;
-
 #if defined(NINTENDO)
 	move_bkg(scroll_x_vblank + (scroll_offset_x << 3), scroll_y_vblank + (scroll_offset_y << 3));
 #elif defined(SEGA)
 	if (_shadow_OAM_OFF == 0) {
-		move_bkg(scroll_x_vblank + (scroll_offset_x << 3), ((UINT16)(scroll_y_vblank + (scroll_offset_y << 3))) % (DEVICE_SCREEN_BUFFER_HEIGHT << 3));
+		__WRITE_VDP_REG_UNSAFE(VDP_RSCX, -(scroll_x_vblank + (scroll_offset_x << 3)));
+		__WRITE_VDP_REG_UNSAFE(VDP_RSCY, (((UINT16)(scroll_y_vblank + (scroll_offset_y << 3))) % (DEVICE_SCREEN_BUFFER_HEIGHT << 3)));
 	}
 #endif
 }
@@ -194,7 +194,7 @@ void main(void) {
 		while (state_running) {
 			if(!vbl_count)
 				wait_vbl_done();
-			delta_time = vbl_count == 1u ? 0u : 1u;
+			delta_time = (vbl_count < 2u) ? 0u : 1u;
 			vbl_count = 0;
 
 			UPDATE_KEYS();
