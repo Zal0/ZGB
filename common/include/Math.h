595810ddcaf7eb89a1ddaf50d4452543f5e8e578
Toxa <untoxa@mail.ru>
2024-04-03 00:14:55 +0300

    optimize the Math.h macros for the modern SDCC

--- common/include/Math.h
+++ common/include/Math.h
@@ -3,17 +3,17 @@
 
 #include <gbdk/platform.h>
 
-#define U_LESS_THAN(A, B) ((A) - (B) & 0x8000u)
+#define U_LESS_THAN(A, B) (((A) < (B)) ? 1 : 0)
 
-#define DISTANCE(A, B) (U_LESS_THAN(A, B) ? (B - A) : (A - B))
+#define DISTANCE(A, B) ((A < B) ? (B - A) : (A - B))
 
-#define SET_BIT(N, POS)   N |=   1 << POS
-#define UNSET_BIT(N, POS) N &= ~(1 << POS)
+#define SET_BIT(N, POS)   ((N) |=  (1 << (POS)))
+#define UNSET_BIT(N, POS) ((N) &= ~(1 << (POS)))
 #define GET_BIT(N, POS)   (1 & DespRight(N, POS))
 
-#define SET_BIT_MASK(N, MASK) N |= MASK
-#define UNSET_BIT_MASK(N, MASK) N &= ~MASK
-#define GET_BIT_MASK(N, MASK) (N & MASK)
+#define SET_BIT_MASK(N, MASK) ((N) |= (MASK))
+#define UNSET_BIT_MASK(N, MASK) ((N) &= ~(MASK))
+#define GET_BIT_MASK(N, MASK) ((N) & (MASK))
 
 inline INT16 DespRight(INT16 a, UINT8 b) {
 	return a >> b;
