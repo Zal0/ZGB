12c367dae5dee67f86f412b5d138cb842e57299b
Toxa <untoxa@mail.ru>
2024-04-03 23:26:03 +0300

    move SGB borders into the target subfolders

--- common/include/main.h
+++ common/include/main.h
@@ -47,6 +47,12 @@ void LCD_isr(void) NONBANKED;
 #define IMPORT_TILES(TILES) extern struct TilesInfo TILES; extern const void __bank_##TILES
 #define IMPORT_FONT(FONT) IMPORT_TILES(FONT)
 
+#ifdef USE_SGB_BORDER
+#define IMPORT_BORDER(MAP) extern struct MapInfo MAP; extern const void __bank_##MAP
+#else
+#define IMPORT_BORDER(MAP) extern void MAP
+#endif
+
 #ifdef SEGA
 #define MAP_OVERLAP_SPR __WRITE_VDP_REG(VDP_R2, R2_MAP_0x3800); __WRITE_VDP_REG(VDP_R5, R5_SAT_0x3F00)
 #define MAP_OVERLAP_BKG __WRITE_VDP_REG(VDP_R2, R2_MAP_0x1800); __WRITE_VDP_REG(VDP_R5, R5_SAT_0x1F00)
