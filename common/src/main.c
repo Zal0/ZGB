5341dd28954e187eb2a3213b6b4a02ec1b9dc42e
Toxa <untoxa@mail.ru>
2024-04-04 21:08:22 +0300

    shorter form for force the linking with the font subsystem

--- common/src/main.c
+++ common/src/main.c
@@ -16,13 +16,9 @@
 #endif
 
 // reference font_recode_table so that allow passing -Wl-g_font_recode_table=ADDR when not using Print()
-void __force_use_font(void) NAKED {
 #if defined(__SDCC)
-	__asm
-		.globl _font_recode_table
-	__endasm;
+static void __force_use_font(void) NAKED { __asm__(".globl _font_recode_table"); }
 #endif
-}
 
 extern UINT8 next_state;
 
