86e9308ee1624b9138de3c47ee7bcc720a3b0a61
Toxa <untoxa@mail.ru>
2024-05-07 21:44:49 +0300

    EXAMPLES: use ZGB screen dimensions definitions

--- examples/print/src/StateGame.c
+++ examples/print/src/StateGame.c
@@ -10,11 +10,11 @@ IMPORT_FONT(font);
 void START(void) {
 	InitScroll(BANK(map), &map, 0, 0);
 	INIT_FONT_EX(font, PRINT_BKG);
-	PRINT((DEVICE_SCREEN_WIDTH - 13) / 2, DEVICE_SCREEN_HEIGHT / 2, "HELLO, WORLD!");
+	PRINT((SCREEN_TILES_W - 13) / 2, SCREEN_TILES_H / 2, "HELLO, WORLD!");
 
 	#ifdef NINTENDO
-	INIT_HUD_EX(map, DEVICE_SCREEN_PX_HEIGHT - 8, 8);
-	print_target=PRINT_WIN;
+	INIT_HUD_EX(map, SCREEN_HEIGHT - 8, 8);
+	print_target = PRINT_WIN;
 	PRINT(0, 0, "OVERLAY %s", "WINDOW");
 	#endif	
 }
